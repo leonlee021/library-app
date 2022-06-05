25b6f23fd6d3c41f8664348bf1f6ded2908e2aa6
leonlee021 <leonlee021@gmail.com>
2022-06-05 13:52:39 +0900

    finish functionality to add new book to library

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title.toString();
-    this.author = author.toString();
-    this.pages = pages.toString();
-    this.read = read.toString();
-    this.info = function(){
-        return title + " by " + author + ", " + pages + " pages, " + read;
-    }
+function Book(title, author, read){
+    this.title = title;
+    this.author = author;
+    this.read = read;
 }
 
-function addBookToLibrary(){
-    const inputBook = new Book(prompt("Title?"), prompt("Author?"), prompt("Pages?"), prompt("Read?"));
-    myLibrary.push({title:inputBook.title, author:inputBook.author, pages:inputBook.pages, read:inputBook.read});
+function addBookToLibrary(input_title, input_author, input_status){
+    myLibrary.push({title:input_title, author:input_author, read:input_status});
 }
-
-addBookToLibrary()
 
 function displayLibrary(){
     for (let i=0; i < myLibrary.length; i++){
         console.log(myLibrary[i]);
     }
 }
+
+let $tableBody = document.querySelector('#table_body');
+
+
+var form = document.forms["form"];
+form.onsubmit = function(e){
+    e.preventDefault();
+    var book_title = document.getElementById("book_title").value;
+    var book_author = document.getElementById('book_author').value;
+    var read_status = document.form.read_status.value;
+    addBookToLibrary(book_title, book_author, read_status);
+    render();
+    console.log(myLibrary);
+}
+
+function render(){
+    $tableBody.innerHTML = '';
+    myLibrary.forEach((book) => {
+        const displayedBook = `
+            <tr>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+            </tr>
+            `;
+        $tableBody.insertAdjacentHTML("afterbegin", displayedBook);
+    });
+}
+
+render();
