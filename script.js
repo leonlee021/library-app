c8685fcc001122a111211d5a4cc8efba263a2bcb
leonlee021 <leonlee021@gmail.com>
2022-06-05 15:36:10 +0900

    add delete button

--- script.js
+++ script.js
@@ -18,6 +18,34 @@ function displayLibrary(){
 
 let $tableBody = document.querySelector('#table_body');
 
+const $table = document
+    .querySelector('table')
+    .addEventListener('click',(e) => {
+        const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+        if (e.target.innerHTML == 'Delete'){
+            if (confirm(`are you sure you want to delete ${currentTarget.innerText}`)){
+                deleteBook(findLibraryIndex(currentTarget.innerText, e.target.parentNode.parentNode.childNodes[3].innerText));
+                render();
+            }
+        }
+    })
+
+function deleteBook(index){
+    myLibrary.splice(index, index + 1);
+}
+
+function findLibraryIndex(book_title, book_author){
+    if (myLibrary.length == 0){
+        return;
+    }
+    else{
+        for (let i = 0; i < myLibrary.length; i++){
+            if (book_title == myLibrary[i].title && book_author == myLibrary[i].author){
+                return i;
+            }
+        }
+    }
+}
 
 var form = document.forms["form"];
 form.onsubmit = function(e){
@@ -27,7 +55,9 @@ form.onsubmit = function(e){
     var read_status = document.form.read_status.value;
     addBookToLibrary(book_title, book_author, read_status);
     render();
-    console.log(myLibrary);
+    document.getElementById("book_title").value = '';
+    document.getElementById('book_author').value = '';
+    document.form.read_status.value = "read";
 }
 
 function render(){
@@ -37,6 +67,8 @@ function render(){
             <tr>
                 <td>${book.title}</td>
                 <td>${book.author}</td>
+                <td><button>${book.read}</button></td>
+                <td><button>Delete</button><td>
             </tr>
             `;
         $tableBody.insertAdjacentHTML("afterbegin", displayedBook);
